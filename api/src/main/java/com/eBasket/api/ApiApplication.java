532882d687aaeeb8340298c50ecbc84b742d1c60
mahadi-hasan16 <mahadi.hasan.khan16@gmail.com>
2026-03-07 03:00:25 +0600

    Enable Auditing

--- api/src/main/java/com/eBasket/api/ApiApplication.java
+++ api/src/main/java/com/eBasket/api/ApiApplication.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 public class ApiApplication {
 
-	public static void main(String[] args) {
+	 static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
 
