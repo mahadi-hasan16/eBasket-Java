41ed484b00329e8dac9992164a1c002b2ff5fd80
mahadi-hasan16 <mahadi.hasan.khan16@gmail.com>
2026-03-07 02:43:47 +0600

    Updated Base Entity

--- api/src/main/java/com/eBasket/api/common/entity/BaseEntity.java
+++ api/src/main/java/com/eBasket/api/common/entity/BaseEntity.java
@@ -21,7 +21,7 @@ public abstract class BaseEntity implements Serializable {
 
     @CreatedDate
     @Column(name = "created_at", nullable = false, updatable = false)
-    private LocalDateTime createdAt = LocalDateTime.now();
+    private LocalDateTime createdAt;
 
     @LastModifiedDate
     @Column(name = "updated_at")
